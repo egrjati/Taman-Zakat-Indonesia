91205aa11e72d67ef355be34a7b5b2285898122e
Zeakeers <168440015+Zeakeers@users.noreply.github.com>
2026-02-27 09:36:15 +0700

    link about

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -36,16 +36,21 @@ export default function Navbar() {
       {/* Redirect */}
       <div className="w-full  px-3 py-2 backdrop-blur md:h-12 md:flex md:flex-col md:justify-center bg-white md:px-6 md:py-0 lg:px-8">
         <ul className="w-full mx-auto flex items-center justify-start gap-2 overflow-x-auto whitespace-nowrap text-xs font-medium text-zinc-700 [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden md:justify-end md:gap-5 md:overflow-visible md:whitespace-normal md:text-base">
-          <li className="group inline-flex shrink-0 cursor-pointer items-center gap-1 rounded-full border border-zinc-200 bg-zinc-50 px-3 py-1 font-semibold transition-all duration-300 hover:-translate-y-0.5 hover:border-[#7FC248] hover:bg-[#F2F9EC] active:scale-[0.98] md:rounded-none md:border-0 md:bg-transparent md:px-0 md:py-0 md:hover:translate-y-0 md:hover:border-transparent md:hover:bg-transparent">
-            Tentang Kami
-            <svg
-              className="h-3.5 w-3.5 text-zinc-500 stroke-2 transition-transform duration-300 group-hover:rotate-180 md:h-4 md:w-5 md:text-black"
-              viewBox="4 2 18 17"
-              fill="none"
-              stroke="currentColor"
+          <li className="shrink-0 flex">
+            <Link
+              href="/about"
+              className="group inline-flex w-full cursor-pointer items-center gap-1 rounded-full border border-zinc-200 bg-zinc-50 px-3 py-1 font-semibold transition-all duration-300 hover:-translate-y-0.5 hover:border-[#7FC248] hover:bg-[#F2F9EC] active:scale-[0.98] md:rounded-none md:border-0 md:bg-transparent md:px-0 md:py-0 md:hover:translate-y-0 md:hover:border-transparent md:hover:bg-transparent"
             >
-              <path d="M6 9l6 6 6-6" />
-            </svg>
+              Tentang Kami
+              <svg
+                className="h-3.5 w-3.5 text-zinc-500 stroke-2 transition-transform duration-300 group-hover:rotate-180 md:h-4 md:w-5 md:text-black"
+                viewBox="4 2 18 17"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M6 9l6 6 6-6" />
+              </svg>
+            </Link>
           </li>
 
           <li className="group inline-flex shrink-0 cursor-pointer items-center gap-1 rounded-full border border-zinc-200 bg-zinc-50 px-3 py-1 font-semibold transition-all duration-300 hover:-translate-y-0.5 hover:border-[#7FC248] hover:bg-[#F2F9EC] active:scale-[0.98] md:rounded-none md:border-0 md:bg-transparent md:px-0 md:py-0 md:hover:translate-y-0 md:hover:border-transparent md:hover:bg-transparent">
